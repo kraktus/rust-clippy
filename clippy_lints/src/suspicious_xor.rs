4b0d9dd6a838add6a93e4098fd8c90014863a171
kraktus <kraktus@users.noreply.github.com>
2022-09-26 11:45:14 +0200

    Refactor to better use `NumericLiteral` utility struct
    I also removed the special case trying to detect if the user wanted the max value of the int, feel free to reintroduce or cherry-pick only the refactor part

--- clippy_lints/src/suspicious_xor.rs
+++ clippy_lints/src/suspicious_xor.rs
@@ -32,58 +32,26 @@ impl LateLintPass<'_> for ConfusingXorAndPow {
             if !in_external_macro(cx.sess(), expr.span);
             if let ExprKind::Binary(op, left, right) = &expr.kind;
             if op.node == BinOpKind::BitXor;
-            if let ExprKind::Lit(litr) = &right.kind;
-            if let ExprKind::Lit(litl) = &left.kind;
-            if let snip_left = snippet(cx, litl.span, "..");
-            if let snip_right = snippet(cx, litr.span, "..");
-            if get_numlit(litr, &snip_right)
-            .zip(get_numlit(litl, &snip_left))
-            .map_or(false, |(a,b)| a.is_decimal() && b.is_decimal());
-            if let left_val = unwrap_lit_to_dec(left).unwrap_or(0);
-            if let right_val = unwrap_lit_to_dec(right).unwrap_or(0);
-            if let suffix = get_numlit(litr, &snip_right).unwrap().suffix.unwrap_or("");
+            if let ExprKind::Lit(lit_left) = &left.kind;
+            if let ExprKind::Lit(lit_right) = &right.kind;
+            if let snip_left = snippet(cx, lit_left.span, "..");
+            if let snip_right = snippet(cx, lit_right.span, "..");
+            if let Some(left_val) = get_numlit(lit_left, &snip_left);
+            if let Some(right_val) = get_numlit(lit_right, &snip_right);
+            if left_val.is_decimal() && right_val.is_decimal();
                 then {
-
-                            if left_val == 2 &&
-                             (right_val == 8 ||
-                            right_val == 16 ||
-                            right_val == 32 ||
-                            right_val == 64 || right_val == 128)
-                            {
-                                clippy_utils::diagnostics::span_lint_and_sugg(
-                                    cx,
-                                    SUSPICIOUS_XOR,
-                                    expr.span,
-                                    "it appears that you are trying to get the maximum value of an integer, but '^' is not exponentiation operator",
-                                    "try with",
-                                    format!("u{right_val}::MAX"),
-                                    Applicability::MaybeIncorrect,
-                                );
-                            } else {
-                                // Even then, warn always.
-                                clippy_utils::diagnostics::span_lint_and_sugg(
-                                    cx,
-                                    SUSPICIOUS_XOR,
-                                    expr.span,
-                                    "'^' is not the exponentiation operator",
-                                    "did you mean to write",
-                                    format!("{left_val}{suffix}.pow({right_val})"),
-                                    Applicability::MaybeIncorrect,
-                                );
-                            }
-
-                    }
+                    // Even then, warn always.
+                    clippy_utils::diagnostics::span_lint_and_sugg(
+                        cx,
+                        SUSPICIOUS_XOR,
+                        expr.span,
+                        "'^' is not the exponentiation operator",
+                        "did you mean to write",
+                        format!("{}.pow({})", left_val.format(), right_val.format()),
+                        Applicability::MaybeIncorrect,
+                    );
+                }
         }
-    }
-}
-
-fn unwrap_lit_to_dec(expr: &Expr<'_>) -> Option<u128> {
-    match &expr.kind {
-        ExprKind::Lit(lit) => match lit.node {
-            LitKind::Int(num, _) => Some(num),
-            _ => None,
-        },
-        _ => None,
     }
 }
 
